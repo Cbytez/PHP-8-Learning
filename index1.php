71f7e32d0f5536f667501c4e246cda7a752ff6fd
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-02-21 23:25:08 -0600

    My first commit and basic operational learning.

---
+++ index1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP 8 Learning</h1>
+    <h2>Hello World</h2>
+</body>
+</html>
+
+<?php
+echo "Hello World";
+echo "<br>";
+
+$name = "Chris";
+$age = 50;
+
+echo "My name is:" . $name . "<br>";
+echo "My age is:" . $age . "<br>";
+
+?>
